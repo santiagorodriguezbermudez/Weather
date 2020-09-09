29c6be553a385eb8ed3f118d0e18f04c249bc824
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-09-09 11:19:03 -0500

    Added main js files for project

---
+++ src/weatherObj.js
@@ -0,0 +1,11 @@
+const Weather = ((city, country, expected, wind, humidity, temp, icon) => (
+  {
+    city,
+    country,
+    expected,
+    wind,
+    humidity,
+    temp,
+    icon,
+  }));
+export default Weather;
